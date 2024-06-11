ac0b62997da85b8e2d99a0ae8593a0cf78358699
Pratishthagpt <pratishthagpt0311@gmail.com>
2024-06-11 08:49:27 +0530

    Initial Commit - created product service

---
+++ productService/src/test/java/dev/pratishtha/project/productService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pratishtha.project.productService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
