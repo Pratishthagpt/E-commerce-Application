632249d6f1a0feffcb3477d69189e0f5933b274a
Pratishthagpt <pratishthagpt0311@gmail.com>
2024-07-04 18:03:12 +0530

    Added JPA Annotations and cardinalities to model classes.

--- userService/src/main/java/dev/pratishtha/project/userService/models/Role.java
+++ userService/src/main/java/dev/pratishtha/project/userService/models/Role.java
@@ -1,5 +1,12 @@
 package dev.pratishtha.project.userService.models;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Role extends BaseModel{
 
     private String role;
--- userService/src/main/java/dev/pratishtha/project/userService/models/User.java
+++ userService/src/main/java/dev/pratishtha/project/userService/models/User.java
@@ -1,13 +1,22 @@
 package dev.pratishtha.project.userService.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@Entity
 public class User extends BaseModel{
 
     private String email;
     private String username;
     private String password;
 
+    @ManyToMany
     private Set<Role> roles = new HashSet<>();
 }
