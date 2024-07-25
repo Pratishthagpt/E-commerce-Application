3f764b5ed2bb202521f570101e4ed2bcabf9997d
Pratishthagpt <pratishthagpt0311@gmail.com>
2024-07-25 19:56:30 +0530

    Added initial version for table creation (V1__init.sql) using flyway.

---
+++ CartService/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE cart
+(
+    id          BINARY(16)   NOT NULL,
+    user_id     VARCHAR(255) NULL,
+    total_price INT NOT NULL,
+    total_items INT NOT NULL,
+    CONSTRAINT pk_cart PRIMARY KEY (id)
+);
+
+CREATE TABLE cart_item
+(
+    id         BINARY(16)   NOT NULL,
+    cart       BINARY(16)   NULL,
+    product_id VARCHAR(255) NULL,
+    quantity   INT NOT NULL,
+    price      INT NOT NULL,
+    CONSTRAINT pk_cartitem PRIMARY KEY (id)
+);
+
+ALTER TABLE cart_item
+    ADD CONSTRAINT FK_CARTITEM_ON_CART FOREIGN KEY (cart) REFERENCES cart (id);
