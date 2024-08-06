5e5ada25c0397390cfeb8384fd3f6dee11cd4761
Pratishthagpt <pratishthagpt0311@gmail.com>
2024-08-06 15:41:11 +0530

    - Added JPA annotations and cardinality between models in payment_service.

--- paymentService/src/main/java/dev/pratishtha/project/paymentService/models/Payment.java
+++ paymentService/src/main/java/dev/pratishtha/project/paymentService/models/Payment.java
@@ -1,12 +1,20 @@
 package dev.pratishtha.project.paymentService.models;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Payment extends BaseModel{
 
     private String orderId;
     private String referenceId;
+
+    @Enumerated(EnumType.ORDINAL)
     private PaymentStatus paymentStatus;
     private int amount;
 }
